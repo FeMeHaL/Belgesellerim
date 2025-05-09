424a9b16baf962e02464b83f79ee0e83d875039a
FeMeHaL <135256364+FeMeHaL@users.noreply.github.com>
2025-05-10 00:15:21 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,28 +1,22 @@
 // data.js
 const allEpisodes = [
-  // SituationCritical
+  // SituationCritical dizisinden bölümler
         {
-          title: "[005] 1. Sezon 5. Bölüm - (Downed Pilot) - Bosna'da Düşen Uçak",
-          date: "İlk Vizyona Girişi : 15 Mayıs 2007",
-          thumbnail: "./img/KA01S05E.jpg",
-          video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/506529168665653",
-          tags: ["Uçak", "F16 Uçağının Düşürülmesi", "1995", "Bosna Hersek", "Pilot Kurtarma Operasyonu"]
+			source: "Kritik Anlar",
+			title: "[005] 1. Sezon 5. Bölüm - (Downed Pilot) - Bosna'da Düşen Uçak",
+			tags: ["Uçak", "F16 Uçağının Düşürülmesi", "1995", "Bosna Hersek", "Pilot Kurtarma Operasyonu"]          
         },
-        {
-          title: "[009] 1. Sezon 9. Bölüm - (Assault On Entebbe) - Uganda’ya Kaçırılan Uçak",
-          date: "İlk Vizyona Girişi : 12 Haziran 2007",
-          thumbnail: "./img/KA01S09E.jpg",
-          video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/1031743588089291",
-          tags: ["Uçak", "Uçak Kaçırma", "1976", "Uganda", "Rehine Operasyonu"]
+		{
+			source: "Kritik Anlar",
+			title: "[009] 1. Sezon 9. Bölüm - (Assault On Entebbe) - Uganda’ya Kaçırılan Uçak",
+			tags: ["Uçak", "Uçak Kaçırma", "1976", "Uganda", "Rehine Operasyonu"]          
         },
 
-  // InterpolInvestigates
-        {
-          title: "<span class='episode-number'>[010]</span> 1. Sezon 10. Bölüm - (Terror in the Skies) - Göklerdeki Terör",
-          date: "<p class=\"episode-date\">İlk Vizyona Girişi : 11 Kasım 2004</p>",
-          thumbnail: "./img/II01S10E.jpg",
-          video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/116231829103937",
-          tags: ["Uçak", "Uçak Kaçırma", "1985", "Yunanistan", "Rehine Operasyonu"]
+// InterpolInvestigates dizisinden bölümler
+		{
+			source: "İnterpol Araştırmaları",
+			title: "[010] 1. Sezon 10. Bölüm - (Terror in the Skies) - Göklerdeki Terör",
+			tags: ["Uçak", "Uçak Kaçırma", "1985", "Yunanistan", "Rehine Operasyonu"]
         }
 
   // Buraya diğer sayfalardan da veri ekleyebilirsin.
