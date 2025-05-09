c5fc974f66e6244adccb8ab424a28612dffb1ff0
FeMeHaL <135256364+FeMeHaL@users.noreply.github.com>
2025-05-09 22:58:32 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,23 +1,29 @@
 // data.js
-const episodesData = {
-  SituationCritical: [
-    {
-      title: "...",
-      date: "...",
-      thumbnail: "...",
-      video: "...",
-      tags: ["Uçak", ...]
-    },
-    ...
-  ],
-  InterpolInvestigates: [
-    {
-      title: "...",
-      date: "...",
-      thumbnail: "...",
-      video: "...",
-      tags: ["Uçak", ...]
-    },
-    ...
-  ]
-};
+const allEpisodes = [
+  // SituationCritical
+  {
+    title: "[001] 1. Sezon 1. Bölüm - (North Hollywood Shootout) - Kuzey Hollywood Çatışması",
+    date: "18 Haziran 2007",
+    thumbnail: "./img/KA01S01E.jpg",
+    video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/575188971570761",
+    tags: ["Polis", "1997", "ABD", "Kuzey Hollywood"]
+  },
+  {
+    title: "[005] 1. Sezon 5. Bölüm - (Downed Pilot) - Bosna'da Düşen Uçak",
+    date: "15 Mayıs 2007",
+    thumbnail: "./img/KA01S05E.jpg",
+    video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/506529168665653",
+    tags: ["Uçak", "F16", "Bosna", "1995"]
+  },
+
+  // InterpolInvestigates
+  {
+    title: "[010] 1. Sezon 10. Bölüm - (Terror in the Skies) - Göklerdeki Terör",
+    date: "11 Kasım 2004",
+    thumbnail: "./img/II01S10E.jpg",
+    video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/116231829103937",
+    tags: ["Uçak", "Uçak Kaçırma", "1985", "Yunanistan"]
+  }
+
+  // Buraya diğer sayfalardan da veri ekleyebilirsin.
+];
--- tagFilter.js
+++ tagFilter.js
@@ -1,3 +1,38 @@
-function filterByTag(tagName) {
-  window.location.href = `/Belgesellerim/filter.html?tag=${encodeURIComponent(tagName)}`;
+// tagFilter.js
+function getTagFromURL() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("tag");
+}
+
+function renderEpisodesByTag(tag) {
+  const container = document.getElementById("results");
+  const header = document.getElementById("header");
+  header.textContent = `"${tag}" etiketine sahip bölümler`;
+
+  const filtered = allEpisodes.filter(ep => ep.tags.includes(tag));
+  if (filtered.length === 0) {
+    container.innerHTML = "<p>Hiç bölüm bulunamadı.</p>";
+    return;
+  }
+
+  filtered.forEach(ep => {
+    const epDiv = document.createElement("div");
+    epDiv.className = "episode";
+    epDiv.innerHTML = `
+      <h3>${ep.title}</h3>
+      <p>${ep.date}</p>
+      <img src="${ep.thumbnail}" alt="${ep.title}" />
+      <div>${ep.tags.map(t => `<span class="tag" onclick="redirectToTag('${t}')">${t}</span>`).join("")}</div>
+    `;
+    container.appendChild(epDiv);
+  });
+}
+
+function redirectToTag(tag) {
+  window.location.href = `filter.html?tag=${encodeURIComponent(tag)}`;
+}
+
+const selectedTag = getTagFromURL();
+if (selectedTag) {
+  renderEpisodesByTag(selectedTag);
 }
