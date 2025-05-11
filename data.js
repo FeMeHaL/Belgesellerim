9a361935390edda6c8499bfa6595ae655d3aea13
FeMeHaL <135256364+FeMeHaL@users.noreply.github.com>
2025-05-11 20:39:38 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
+let dynamicViews = "750"; // Başlangıçta dinamik izlenme sayısını saklamak için bir yer ayarlıyoruz
+
 const allEpisodes = {
   "İnterpol Araştırmaları": {
     1: [
@@ -7,20 +9,28 @@ const allEpisodes = {
         thumbnail: "./img/II01S10E.jpg",
         video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/116231829103937",
         tags: ["Uçak", "Uçak Kaçırma", "1985", "Yunanistan", "Rehine Operasyonu"],
-        views: "buraya yazılacak !"
+        views: dynamicViews // Dinamik değer burada kullanılacak
       }
     ]
   }
 };
 
-// Sayfa kaynağından alınan tüm veri buraya string olarak gelecek:
+// Sayfa kaynağından alınan tüm veri buraya string olarak gelecek (örnek veri)
 const sourceCode = `view-source:https://www.facebook.com/100021513607216/videos/116231829103937/`;
 
-// İlk "is_live_streaming":false ile başlayan JSON parçasını bul
+// Sayfa kaynağında "is_live_streaming":false ile başlayan JSON parçasını arıyoruz
 const match = sourceCode.match(/({\s*"is_live_streaming":false[\s\S]+?"is_play_count_supported":false\s*})/);
 
 if (match) {
-  const jsonStr = match[1];
-  const jsonObj = JSON.parse(jsonStr);
-  allEpisodes["İnterpol Araştırmaları"][1][0].views = jsonObj.play_count;
+  const jsonStr = match[1]; // JSON verisini alıyoruz
+  const jsonObj = JSON.parse(jsonStr); // JSON stringini parse ediyoruz
+  
+  // Burada jsonObj.play_count değerini alıp, dynamicViews değişkenine atıyoruz
+  dynamicViews = jsonObj.play_count;
+
+  // Son olarak allEpisodes objesindeki views kısmını güncelliyoruz
+  allEpisodes["İnterpol Araştırmaları"][1][0].views = dynamicViews;
 }
+
+// Dinamik veriyi HTML sayfasına yansıtmak için:
+document.querySelector('.views').innerText = dynamicViews; // Buradaki .views sınıfına sahip elemanın içeriğini güncelliyoruz
