1b44a09c086ddd9e433454ee0d9d846a67a56d97
FeMeHaL <135256364+FeMeHaL@users.noreply.github.com>
2025-05-11 20:07:26 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ const allEpisodes = {
         thumbnail: "./img/KA01S05E.jpg",
         video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/506529168665653",
         tags: ["Uçak", "F16 Uçağının Düşürülmesi", "1995", "Bosna Hersek", "Pilot Kurtarma Operasyonu"],
-        views: 0 // İzlenme sayısı başlangıçta 0
+        views: 0 // Başlangıçta 0
       },
       {
         title: "[009] 1. Sezon 9. Bölüm - (Assault On Entebbe) - Uganda’ya Kaçırılan Uçak",
@@ -16,7 +16,7 @@ const allEpisodes = {
         thumbnail: "./img/KA01S09E.jpg",
         video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/1031743588089291",
         tags: ["Uçak", "Uçak Kaçırma", "1976", "Uganda", "Rehine Operasyonu"],
-        views: 0 // İzlenme sayısı başlangıçta 0
+        views: 0 // Başlangıçta 0
       }
     ]
   },
@@ -29,7 +29,15 @@ const allEpisodes = {
         thumbnail: "./img/II01S10E.jpg",
         video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/116231829103937",
         tags: ["Uçak", "Uçak Kaçırma", "1985", "Yunanistan", "Rehine Operasyonu"],
-        views: "buraya yazılacak !" // Bu alanda dinamik veriyi göstereceğiz
+        views: "buraya yazılacak !" // Burada dinamik veriyi yazacağız
+      },
+      {
+        title: "İnterpol Araştırmaları<br><span class='episode-number'>[011]</span> 1. Sezon 11. Bölüm<br>(Göklerdeki Korku) - Bir Uçak Kaçırma Operasyonu",
+        date: "<p class=\"episode-date\">İlk Vizyona Girişi : 25 Aralık 2004</p>",
+        thumbnail: "./img/II01S11E.jpg",
+        video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/1376284733513700",
+        tags: ["Uçak", "Uçak Kaçırma", "1986", "Almanya", "Rehine Operasyonu"],
+        views: "buraya yazılacak !" // Burada da dinamik veriyi yazacağız
       }
     ],
     seasonYears: {
@@ -39,8 +47,9 @@ const allEpisodes = {
 };
 
 // Dinamik veri çekme ve güncelleme işlemi
-// Video 1
-let video1 = document.querySelector('div[data-ft*="116231829103937"]'); // İlk video
+
+// Video 1 (ID: 116231829103937)
+let video1 = document.querySelector('div[data-ft*="116231829103937"]'); 
 if (video1) {
   let playCount1 = video1.innerHTML.match(/"play_count":(\d+)/);
   if (playCount1) {
@@ -48,8 +57,8 @@ if (video1) {
   }
 }
 
-// Video 2
-let video2 = document.querySelector('div[data-ft*="1376284733513700"]'); // İkinci video
+// Video 2 (ID: 1376284733513700)
+let video2 = document.querySelector('div[data-ft*="1376284733513700"]');
 if (video2) {
   let playCount2 = video2.innerHTML.match(/"play_count":(\d+)/);
   if (playCount2) {
@@ -57,5 +66,5 @@ if (video2) {
   }
 }
 
-// Güncellenmiş 'views' değerini yazdırma
+// Sonucu yazdırma: Burada `views` değerini HTML sayfasında dinamik olarak göstereceğiz.
 document.getElementById("result").innerHTML = JSON.stringify(allEpisodes, null, 2);
