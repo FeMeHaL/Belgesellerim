2e3d4136eb6aecc2fa3a3522a6a18ff2bf3f3cea
FeMeHaL <135256364+FeMeHaL@users.noreply.github.com>
2025-05-11 21:12:25 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,40 +1,57 @@
-let dynamicViews = "750"; // Başlangıçta dinamik izlenme sayısını saklamak için bir yer ayarlıyoruz
-
-const allEpisodes = {
-  "İnterpol Araştırmaları": {
-    1: [
-      {
-        title: "İnterpol Araştırmaları<br><span class='episode-number'>[010]</span> 1. Sezon 10. Bölüm<br>(Terror in the Skies) - Göklerdeki Terör",
-        date: "<p class=\"episode-date\">İlk Vizyona Girişi : 23 Kasım 2004</p>",
-        thumbnail: "./img/II01S10E.jpg",
-        video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/116231829103937",
-        tags: ["Uçak", "Uçak Kaçırma", "1985", "Yunanistan", "Rehine Operasyonu"],
-        views: dynamicViews // Dinamik değer burada kullanılacak
-      }
-    ]
+// Tüm videolar için izlenme sayısını almak için genel bir fonksiyon
+async function fetchVideoViews(videoUrl) {
+  try {
+    const response = await fetch(videoUrl); // Videonun sayfasından veriyi alıyoruz (bu API veya benzeri bir yapı ile olabilir)
+    const sourceCode = await response.text(); // Sayfa kaynağını alıyoruz
+    
+    // "is_play_count_supported": false kısmından play_count değerini alıyoruz
+    const match = sourceCode.match(/"is_play_count_supported":\s*false[\s\S]+?"play_count":\s*(\d+)/);
+    
+    if (match) {
+      return match[1]; // play_count değerini döndürüyoruz
+    } else {
+      throw new Error("Play count verisi bulunamadı.");
+    }
+  } catch (error) {
+    console.error("Video verisi alınırken hata oluştu:", error);
+    return null;
   }
-};
-
-// Sayfa kaynağından alınan tüm veri buraya string olarak gelecek (örnek veri)
-const sourceCode = `view-source:https://www.facebook.com/100021513607216/videos/116231829103937/`;
-
-// Sayfa kaynağında "is_play_count_supported":false ile başlayan JSON parçasını arıyoruz
-const match = sourceCode.match(/"is_play_count_supported":false\s*}\s*"play_count":(\d+)/);
-
-
-if (match) {
-  // JSON verisini alıyoruz
-  const jsonStr = match[1];
-  
-  // JSON stringini parse ediyoruz
-  const jsonObj = JSON.parse(jsonStr);
-  
-  // jsonObj'den play_count değerini alıyoruz
-  dynamicViews = jsonObj.play_count;
-
-  // Son olarak allEpisodes objesindeki views kısmını güncelliyoruz
-  allEpisodes["İnterpol Araştırmaları"][1][0].views = dynamicViews;
 }
 
-// Dinamik veriyi HTML sayfasına yansıtmak için:
-document.querySelector('.views').innerText = dynamicViews; // Buradaki .views sınıfına sahip elemanın içeriğini güncelliyoruz
+// Tüm bölümleri güncelleme fonksiyonu
+async function updateVideoViews() {
+  const allEpisodes = {
+    "İnterpol Araştırmaları": {
+      1: [
+        {
+          title: "İnterpol Araştırmaları<br><span class='episode-number'>[010]</span> 1. Sezon 10. Bölüm<br>(Terror in the Skies) - Göklerdeki Terör",
+          date: "<p class=\"episode-date\">İlk Vizyona Girişi : 23 Kasım 2004</p>",
+          thumbnail: "./img/II01S10E.jpg",
+          video: "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ferhat.polaterce.9/videos/116231829103937",
+          tags: ["Uçak", "Uçak Kaçırma", "1985", "Yunanistan", "Rehine Operasyonu"],
+          views: "750" // Dinamik değer burada kullanılacak
+        }
+      ]
+    }
+  };
+  
+  const episodes = allEpisodes["İnterpol Araştırmaları"][1]; // Tüm bölümleri alıyoruz
+  
+  // Tüm bölümler için views değerlerini güncelleme
+  for (const episode of episodes) {
+    const videoUrl = episode.video; // Videonun URL'sini alıyoruz
+    const views = await fetchVideoViews(videoUrl); // Videonun izlenme sayısını alıyoruz
+    
+    if (views) {
+      episode.views = views; // İzlenme sayısını güncelliyoruz
+    }
+  }
+  
+  // Güncellenmiş verileri HTML'e yansıtmak için
+  episodes.forEach(episode => {
+    document.querySelector(`.views[data-video="${episode.video}"]`).innerText = episode.views;
+  });
+}
+
+// Başlatmak için
+updateVideoViews();
